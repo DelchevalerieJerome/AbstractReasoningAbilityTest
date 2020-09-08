1020058433d23bbdd1c26e9905701c969d4a6c4f
Jerome Delchevalerie <jerome0delche0@gmail.com>
2020-09-08 13:30:19 +0200

    Initialisation du projet

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<style>
+			h1 { 
+				color: black;
+			}
+		</style>
+		<!-- <script src="Exercice5JStest.js"></script> -->
+	</head>
+	<body>
+		<h1> Accueil </h1>
+		<p>
+			<button id="bNewTest">Commencer un nouveau test</button>
+			<button id="bStat">Mes résultats</button>
+			<button id="bLogOut" >Se déconnecter</button>
+		</p>
+	</body>
+</html>
